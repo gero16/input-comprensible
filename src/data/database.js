7296b9cf4a04d8cdcd2a0857de2dd9b3a81dceef
Geronimo Nicola <geronicola1696@gmail.com>
2023-07-25 01:49:54 -0300

    carpeta audios en public

--- src/data/database.js
+++ src/data/database.js
@@ -1,27 +1,27 @@
 
 
 export const listaScream = [
-    ["Do you remember the beginning?", "../src/audios/scream/Grabacion.m4a", "easy"], 
-    ["How well do you remember the original?", "../src/audios/scream/Grabacion2.m4a", "easy"],
-    ["She answers the wrong number and starts talking", "../src/audios/scream/Grabacion3.m4a", "medium"],
-    ["Amber's looking particularly fetching tonight", "../src/audios/scream/Grabacion4.m4a", "medium"],
-    ["I told you, i want to play a game", "../src/audios/scream/Grabacion5.m4a", "easy"],
-    [" Hey, what's going on?", "../src/audios/scream/Grabacion6.m4a", "easy"],
-    [" What do you think? You're the expert", "../src/audios/scream/Grabacion7.m4a", "easy"]
+    ["Do you remember the beginning?", "./audios/scream/Grabacion.m4a", "easy"], 
+    ["How well do you remember the original?", "./audios/scream/Grabacion2.m4a", "easy"],
+    ["She answers the wrong number and starts talking", "./audios/scream/Grabacion3.m4a", "medium"],
+    ["Amber's looking particularly fetching tonight", "./audios/scream/Grabacion4.m4a", "medium"],
+    ["I told you, i want to play a game", "./audios/scream/Grabacion5.m4a", "easy"],
+    [" Hey, what's going on?", "./audios/scream/Grabacion6.m4a", "easy"],
+    [" What do you think? You're the expert", "./audios/scream/Grabacion7.m4a", "easy"]
 ]
 
 export const listaBojack = [
-    ["Yeah great, get me away from the cameras?", "./src/audios/bojack/Grabacion1.m4a", "medium"], 
-    ["...", "../src/audios/bojack/Grabacion2.m4a", "medium"],
-    ["...", "../src/audios/bojack/Grabacion3.m4a", "medium"],
-    ["...", "../src/audios/bojack/Grabacion4.m4a", "medium"],
-    ["...?", "../src/audios/bojack/Grabacion5.m4a", "hard"],
-    ["...", "../src/audios/bojack/Grabacion6.m4a", "easy"],
+    ["Yeah great, get me away from the cameras?", "/audios/bojack/Grabacion1.m4a", "medium"], 
+    ["...", "./audios/bojack/Grabacion2.m4a", "medium"],
+    ["...", "./audios/bojack/Grabacion3.m4a", "medium"],
+    ["...", "./audios/bojack/Grabacion4.m4a", "medium"],
+    ["...?", "./audios/bojack/Grabacion5.m4a", "hard"],
+    ["...", "./audios/bojack/Grabacion6.m4a", "easy"],
 ]
 
 export const listaSimpsons = [
-    [" Yeah give it a break doctor", "./src/audios/simpsons/Grabacion1.m4a"], 
-    ["What's matter", "../src/audios/simpsons/Grabacion2.m4a"]
+    [" Yeah give it a break doctor", "/audios/simpsons/Grabacion1.m4a"], 
+    ["What's matter", "./audios/simpsons/Grabacion2.m4a"]
 ]
 
 export const secretInvasion = [
