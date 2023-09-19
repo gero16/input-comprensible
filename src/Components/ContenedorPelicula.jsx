4c4d99ff95a4c558c43637ac17e57b483a4e5e63
gero <geronicola1696@gmail.com>
2023-09-18 21:03:35 -0300

    urlBackend en ContenedorPelicula

--- src/Components/ContenedorPelicula.jsx
+++ src/Components/ContenedorPelicula.jsx
@@ -7,16 +7,20 @@ import Pelicula from './Pelicula';
 
 const ContenedorPelicula = () => {
     let { pelicula } = useParams();
-
+    const urlBackend = import.meta.env.VITE_URL_BACKEND
     const [data, setData] = useState([])
 
     // LA IDEA ES QUE ESTO SE EJECUTE SOLO UNA PRIMERA VEZ PARA LLENAR LOCALSTORAGE
 
-    const fetchData = () =>{
-        const { peliculas } = ListaVideos
-        const encontrarPelicula = peliculas.find((e) => e.subtitulo == pelicula)
-        setData(encontrarPelicula)
-        return data
+    const fetchData = async () =>{
+        
+        const response = await fetch(`${ urlBackend }/pelicula/${ pelicula }`);
+        const resp= await response.json();
+        console.log(resp)
+        if(!resp) console.log("No hay data")
+
+        setData(resp.data)
+        return data;
     }
 
     useEffect(() => {
