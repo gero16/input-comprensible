ceec5e4dbeb0290baf589329d91d9a885ddd0a0f
geronimo <geronicola1696@gmail.com>
2024-04-04 20:09:59 -0300

    validaciones en titulos

--- src/context/titulos.js
+++ src/context/titulos.js
@@ -18,19 +18,26 @@ export const fetchTitulos = async (titulos, setTitulos) => {
                     'Access-Control-Allow-Origin': urlOrigin,
                 }),
             })
-        const resp= await response.json();
-        //console.log(resp.data)
-        if(!resp) console.log("No hay data")
 
-        resp.data.forEach(element => {
-            const titulo = [element.titulo, element.subtitulo, element.categoria, element.temporada, element.capitulo, element.imagen]
-            arrayTitulos.push(titulo)
-           console.log(arrayTitulos)
-        });
-        //console.log(resp.data)
-        setTitulos(arrayTitulos)
-        //console.log(arrayTitulos)
-        return titulos
+            if(response.status === 200) {
+                const resp = await response.json();
+                console.log(resp)
+                if(!resp) console.log("No hay data")
+                
+                console.log(response)
+                resp.data.forEach(element => {
+                    const titulo = [element.titulo, element.subtitulo, element.categoria, element.temporada, element.capitulo, element.imagen]
+                    arrayTitulos.push(titulo)
+             
+                });
+               
+                setTitulos(arrayTitulos)
+                //console.log(arrayTitulos)
+                return titulos  
+
+            }
+            if(!response) console.log("Peticion a url equivocada")
+            if(response.status === 404) console.log("No hay data")
         
     } catch (error) {
 
