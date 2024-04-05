99ca94f82ab717ecb393b3114d89f5ba3c4db7e0
geronimo <geronicola1696@gmail.com>
2024-04-05 01:16:30 -0300

    cambios css en index

--- src/Components/Index/InputInfo.jsx
+++ src/Components/Index/InputInfo.jsx
@@ -1,6 +1,6 @@
 import Fondo from "../../assets/fondo.jpg"
 import Fondo2 from "../../../public/harry-potter-1-portada.jpg" 
-
+import Fondo3 from "../../../public/bastardos-sin-gloria-portada.jpg" 
 
 const InputInfo = () => {
     return (
@@ -13,8 +13,8 @@ const InputInfo = () => {
 
           <ul className='ul-contenido flex-column gap-50'>
 
-            <li className="flex wrap">
-              <div className="div-contenido-index flex-column-center gap-20">
+            <li className="flex wrap fondo-li p-30">
+              
                 <p className="p-index"> En esta página podrás encontrar un lugar donde practicar el input comprensible
                     mediante clips cortos de series y peliculas 
                 </p>
@@ -26,17 +26,16 @@ const InputInfo = () => {
                   Esta teoria se refiere a la idea de que para que una persona aprenda un idioma de manera efectiva, el material 
                   de entrada o input (como textos, conversaciones, videos, etc.) debe ser comprensible para el mismo.
                 </p>
-              </div>
-              <img src={Fondo} alt="" className="foto-index" />
-            </li>
-
-            <li className="flex wrap">
-
-              <img src={Fondo2} alt="" className="foto-index" />
-              <div className="div-contenido-index flex-column-center gap-10">
+              
                 <p className="p-index">  Es decir, se entiende por input comprensible todo aquella entrada de información que reprepresente un 
                     nivel lingüístico ligeramente por encima del conocimiento actual del estudiante.
                 </p>
+         
+            </li>
+
+            <li className="flex wrap fondo-li-2 p-30">
+
+           
                 <p className="p-index">
                 En otras palabras, que debe ser lo suficientemente desafiante para que el estudiante tenga 
                     que esforzarse para entenderlo, pero no tan difícil como para que resulte incomprensible 
@@ -48,43 +47,30 @@ const InputInfo = () => {
                     desarrollo próximo". Zona en la que el aprendizaje es más efectivo, debido a que el estudiante está 
                     en condiciones de adquirir nuevo conocimiento y construir sobre lo que ya conoce.
                 </p>
-              </div>
+              
+                <p className="p-index"> Por lo que su práctica fomentaria la adquisición natural del lenguaje a través de la exposición constante 
+                  y gradual a estructuras lingüísticas más complejas. </p>
               
             </li>
     
       
-            <li className="flex-center div-meme" >
-              <div>
+            <li className="flex wrap fondo-li-3 p-30" >
             
-                <p className="p-index"> Por lo que su práctica fomentaria la adquisición natural del lenguaje a través de la exposición constante 
-                  y gradual a estructuras lingüísticas más complejas. </p>
-              </div>
-  
-            </li>
-
-            <li className="flex-center div-meme">
-    
-
-                <div>
+       
                   <p className="p-index">   Para clasificar la dificultad de comprensión de los clips se tomaron en cuenta diferentes factores.
                       Entre ellos estan la complejidad de la situación que representan, la forma en la que los actores se expresan, sus modismos, etc. </p>
                   <p className="p-index"> Para que la actividad resulte efectiva se recomienda buscar clips de series/peliculas que puedan estar acorde a su nivel.  </p>
-                  <p className="p-index"> Asi como ya haber visto la pelicula o el capitulo de su interés. Para tener el contexto suficiente para la comprensión del mismo</p>
-                  
-                </div>
-      
-            </li>
+                  <p className="p-index"> Asi como ya haber visto la pelicula o el capitulo de su interés. Para tener el contexto suficiente para la comprensión del mismo </p>
 
-            <li className="flex-center div-meme">
-            <div>
               <p className="p-index">
                 En cada clip se presenta la posibilidad de evaluar su comprensión escribiendo en su campo correspondiente y seleccionado el boton 'Evaluar'. 
         
               </p>
               <p className="p-index"> También se puede practicar el output correspondiente al clip grabando un audio, que puede guardar o actualizar. </p>
-            </div>
+          
+              </li>
             
-            </li>
+
           </ul>
 
         </main>
