86077597e10e219bcc5cfa1b0993aa50fda8dd0c
geronimo <geronicola1696@gmail.com>
2024-04-18 18:09:40 -0300

    css

--- src/Components/Index/InputInfo.jsx
+++ src/Components/Index/InputInfo.jsx
@@ -6,14 +6,14 @@ const InputInfo = () => {
     return (
         <div className='fondo'>
           <div className="div-titulo">
-            <h1 className='titulo-principal'> Input Comprensible </h1>
+            <h1 className='titulo-principal arcoiris'> Input Comprensible </h1>
           </div>
 
         <main className="main" >
 
           <ul className='ul-contenido flex-column gap-50'>
 
-            <li className="flex wrap li-index fondo-li p-30">
+            <li className="flex wrap li-index fondo-li p-30 flex-column-center">
               
                 <p className="p-index"> En esta página podrás encontrar un lugar donde practicar el input comprensible
                     mediante clips cortos de series y peliculas 
@@ -33,42 +33,48 @@ const InputInfo = () => {
          
             </li>
 
-            <li className="flex wrap li-index fondo-li-2 p-30">
+            <li className="flex wrap li-index fondo-li-2 p-30 flex-column-center">
 
-           
                 <p className="p-index">
-                En otras palabras, que debe ser lo suficientemente desafiante para que el estudiante tenga 
+                    En otras palabras, que debe ser lo suficientemente desafiante para que el estudiante tenga 
                     que esforzarse para entenderlo, pero no tan difícil como para que resulte incomprensible 
                     o abrumador.
                 </p>
                 <p className="p-index">
                   La idea del concepto propone que cuando un estudiante se expone a una entrada de información
-                    un poco más avanzada que su nivel actual de competencia, se encuentra en una "zona de 
-                    desarrollo próximo". Zona en la que el aprendizaje es más efectivo, debido a que el estudiante está 
-                    en condiciones de adquirir nuevo conocimiento y construir sobre lo que ya conoce.
+                  un poco más avanzada que su nivel actual de competencia, se encuentra en una "zona de 
+                  desarrollo próximo". Zona en la que el aprendizaje es más efectivo, debido a que el estudiante está 
+                  en condiciones de adquirir nuevo conocimiento y construir sobre lo que ya conoce.
                 </p>
               
-                <p className="p-index"> Por lo que su práctica fomentaria la adquisición natural del lenguaje a través de la exposición constante 
-                  y gradual a estructuras lingüísticas más complejas. </p>
+                <p className="p-index"> Por lo que su práctica fomentaria la adquisición natural del lenguaje a través de la exposición 
+                  constante y gradual a estructuras lingüísticas más complejas. 
+                </p>
               
             </li>
     
       
-            <li className="flex wrap li-index fondo-li-3 p-30" >
+            <li className="flex wrap li-index fondo-li-3 p-30 flex-column-center" >
             
-       
-                  <p className="p-index">   Para clasificar la dificultad de comprensión de los clips se tomaron en cuenta diferentes factores.
-                      Entre ellos estan la complejidad de la situación que representan, la forma en la que los actores se expresan, sus modismos, etc. </p>
-                  <p className="p-index"> Para que la actividad resulte efectiva se recomienda buscar clips de series/peliculas que puedan estar acorde a su nivel.  </p>
-                  <p className="p-index"> Asi como ya haber visto la pelicula o el capitulo de su interés. Para tener el contexto suficiente para la comprensión del mismo </p>
+              <p className="p-index">   Para clasificar la dificultad de comprensión de los clips trare de basarme en mi propia comprension 
+                de los modismos pero teniendo también en cuenta en cuenta diferentes factores. Entre ellos estan la complejidad de la 
+                situación que  representan, la forma en la que los actores se expresan, sus modismos, etc. 
+              </p>
+              <p className="p-index"> Para que la actividad resulte efectiva se recomienda buscar clips de series/peliculas que puedan 
+                estar acorde a su nivel. Asi como haber visto la pelicula o el capitulo de su interés con anterioridad. De forma de tener
+                  un mejor contexto ayude en su omprensión 
+              </p>
 
               <p className="p-index">
-                En cada clip se presenta la posibilidad de evaluar su comprensión escribiendo en su campo correspondiente y seleccionado el boton 'Evaluar'. 
-        
+               Para cada clip se recomienda escucharlo y en base a lo entendido grabar un audio para compararse con el original. Tambien 
+               puede evaluarsu comprension escribiendo en el campo de texto correspondiente y tocar el boton "Evaluar" para verificarlo.
               </p>
-              <p className="p-index"> También se puede practicar el output correspondiente al clip grabando un audio, que puede guardar o actualizar. </p>
+
+              <p className="p-index"> La opcion "Mostrar Respuesta" siempre esta disponible. Y las grabacaciones pueden quedar guardadas 
+                tanto como actualizarse o borrarse. 
+              </p>
           
-              </li>
+            </li>
             
 
           </ul>
