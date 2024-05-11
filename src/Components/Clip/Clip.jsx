4a735e1dc6ca32b0d5463eff26e9b09cd17f5753
geronimo <geronicola1696@gmail.com>
2024-05-11 02:15:28 -0300

    arreglos css en portadas de index

--- src/Components/Clip/Clip.jsx
+++ src/Components/Clip/Clip.jsx
@@ -5,67 +5,55 @@ import { Context } from "../../context/context"
 import "./Clip.css"
 import { urlBackend_Desarrollo, urlBackend_Produccion } from "../../context/helpers";
 
-const opts = { height: '400', width: '800', playerVars: { autoplay: 1,}, };
+const Clip = ({ id, imagen, categoria, subtitulo, video, index, frase, dificultad, capitulo, grabacionID, numero_clip }) => {
 
-
-const Clip = ({ id, imagen, categoria, subtitulo, video, index, frase, dificultad, 
-                capitulo, grabacionID, numero_clip }) => {
-
-    const { clickGrabar, evaluar, mostrarRespuesta, addAudioElement, transformarMayuscula } = useContext(Context)
-    let {  usuario, temporada, serie } = useParams();
-    const recorderControls = useAudioRecorder()
-
+    const { clickGrabar, evaluar, mostrarRespuesta, addAudioElement, transformarMayuscula } = useContext(Context);
+    const { usuario, temporada, serie } = useParams();
+    const recorderControls = useAudioRecorder();
     const [width, setWidth] = useState(window.innerWidth);
+    const separarDificultad = dificultad.split("-");
+    const primeraPalabra = separarDificultad[0].charAt(0).toUpperCase() + separarDificultad[0].slice(1);
+    const segundaPalabra = dificultad.includes("-") ? separarDificultad[1].charAt(0).toUpperCase() + separarDificultad[1].slice(1) : " ";
+    const newDificultad = dificultad.includes("-") ? `${primeraPalabra} ${segundaPalabra}` : primeraPalabra;
+    const [grabacion, setGrabacion] = useState("");
 
-    const separarDificultad = dificultad.split("-")
-    const primeraPalabra = separarDificultad[0].charAt(0).toUpperCase() + separarDificultad[0].slice(1)
-    const segundaPalabra = dificultad.includes("-") ? separarDificultad[1].charAt(0).toUpperCase() + separarDificultad[1].slice(1) : " "
-    const newDificultad = dificultad.includes("-") ? `${primeraPalabra} ${segundaPalabra}` : primeraPalabra
-    
-    const [grabacion, setGrabacion] = useState([])
-  
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setWidth(window.innerWidth);
+        });
+    }, []);
+
     const guardarGrabacion = async (elemento, indice) => {
-        const urlBlob = elemento.firstElementChild.src
+        const urlBlob = elemento.firstElementChild.src;
         const response = await fetch(urlBlob);
         if (response.ok) {
             const blob = await response.blob();
-   
             const formData = new FormData()
             formData.append("grabacion", blob);
             formData.append("subtitulo", subtitulo);
             formData.append("id_clip", id);
             formData.append("numero_clip", indice);
-            
             const urlGuardarGrabacion =  serie 
-                ? `${ urlBackend_Produccion }/agregar-grabacion/series/${subtitulo}/${temporada}/${capitulo}/${usuario}`
-                : `${ urlBackend_Produccion }/agregar-grabacion/peliculas/${ subtitulo }/${ usuario }`
-            
-            console.log(urlGuardarGrabacion)
-            const resultado = fetch(urlGuardarGrabacion, { method: "POST", body: formData });
-            resultado.then(() => {
-
-                console.log(resultado)
-            })
-
-            //window.localStorage.clear();
-        
+                ? `${ urlBackend_Desarrollo }/agregar-grabacion/series/${subtitulo}/${temporada}/${capitulo}/${usuario}`
+                : `${ urlBackend_Desarrollo }/agregar-grabacion/peliculas/${ subtitulo }/${ usuario }`;
+            const resultado = await fetch(urlGuardarGrabacion, { method: "POST", body: formData });
+            const result = await resultado.json()
+            console.log(result)
         } else {
             console.error('Error al obtener el archivo de audio:', response.statusText);
         }
-    }
+    };
 
     const actualizarGrabacion = async (elemento, indice) => {
-        const url = 
-            categoria === "serie"
+        const url = categoria === "serie"
             ? `${ urlBackend_Produccion }/actualizar-grabacion/series/${subtitulo}/${temporada}/${usuario}`
-            : `${ urlBackend_Produccion }/actualizar-grabacion/peliculas/${subtitulo}/${usuario}`
-            
+            : `${ urlBackend_Produccion }/actualizar-grabacion/peliculas/${subtitulo}/${usuario}`;
         const objetoGrabacion = {
-                "fecha": "2023-09-27",
-                "grabacion":  elemento.firstElementChild.src, 
-                "subtitulo": `${subtitulo}`,
-                "id_clip": id
-        }
+            "fecha": "2023-09-27",
+            "grabacion":  elemento.firstElementChild.src, 
+            "subtitulo": `${subtitulo}`,
+            "id_clip": id
+        };
         const response = await fetch(url,  
             {
                 method: 'PUT',
@@ -75,71 +63,63 @@ const Clip = ({ id, imagen, categoria, subtitulo, video, index, frase, dificulta
                     'Access-Control-Allow-Origin': '*',
                 }),
                 body : JSON.stringify(objetoGrabacion)
-            })
+            });
         const resp= await response.json();
         if(!resp) console.log("No hay data")
-        
-    }
-   
-    
-    useEffect(() => {
-        setGrabacion(grabacionID)
-    }, [])
+    };
+
+    const onPlayerStateChange = (event) => {
+        console.log(event)
+        /*
+        if (event.data === window.YT.PlayerState.PLAYING) {
+            const duration = event.target.getDuration();
+            console.log(duration)
+            setTimeout(() => {
+                event.target.pauseVideo();
+            }, (duration - 3) * 1000);
+        }
+        */
+    };
 
     return (
         <>
             <article className={`article-video`} id={`id-BD-${id}`}>
-            
-            {
-                
-                <h3> Id {id} - Numero de clip {numero_clip}  </h3> 
-                
-            }
                 { imagen ?  <img src={imagen} alt="imagen portada" /> : <> </> }
-               
                 <section className="section-video">
-
                     <section className="section-audio">
-                            
                         <span className={`ocultar ${ subtitulo }-${ index } frase`} > { frase } </span>
                         <span className={`ocultar ${subtitulo}-mostrar-${index}` }> Incorrecto! </span>
-
                         <span className={`bold ${dificultad} `}> {newDificultad} </span>
                         <input type="text" className={`input-${ subtitulo }-${ index } input-frase`} />
                         <input type="text" className={`ocultar inputRespuesta-${ subtitulo }-${ index } `} defaultValue={ frase} />
-
                         <section className="flex-between">
                             <button className="button" onClick={(e) => evaluar(subtitulo, index)} id="btn-evaluar">Evaluar</button>
-
                             <button className="button" onClick={(e) => mostrarRespuesta(subtitulo, index)} id="btn-mostar-respuesta"> Mostrar Respuesta </button>
                         </section>
-
                     </section>
-
                     <section id={`grabar-${ subtitulo }-${ index } section-video`} onClick={(e) => clickGrabar(e.target)}>
-                            
-                      { width < 540 
-                        ? <iframe 
-                            width={"300"} 
-                            height={"160"} 
-                            src={`${video}`}
-                            title="YouTube video player"  
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                            loading="lazy"
-                            className="arcoiris"
-                        />
-                        
-                        : <iframe 
-                            width={ width <= 1511 ? "410" : "450"} 
-                            height={ width <= 1511 ? "210" : "260"} 
-                            src={`${video}`}
-                            title="YouTube video player"  
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                            loading="lazy"
-                             className="arcoiris"
-                        />
-                      }
-                        
+                        { width < 540 
+                            ? <iframe 
+                                width={"300"} 
+                                height={"160"} 
+                                src={`${video}`}
+                                title="YouTube video player"  
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                loading="lazy"
+                                className="arcoiris"
+                                onReady={(event) => onPlayerStateChange(event)}
+                            />
+                            : <iframe 
+                                width={ width <= 1511 ? "410" : "450"} 
+                                height={ width <= 1511 ? "210" : "260"} 
+                                src={`${video}`}
+                                title="YouTube video player"  
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                loading="lazy"
+                                className="arcoiris"
+                                onReady={(event) => onPlayerStateChange(event)}
+                            />
+                        }
                         <div className={ width > 1300 ? "flex-center" : ""}>
                             <span className="grabar-audio"> { width > 1300 ? "Grabar Audio" : "Grabar Audio -"} </span>
                             <AudioRecorder 
@@ -157,62 +137,44 @@ const Clip = ({ id, imagen, categoria, subtitulo, video, index, frase, dificulta
                                     AudioRecorderStartSaveClass : `audio-mic-${index} audio-mic-${subtitulo}`,
                                 }} 
                             />
-                    
                         </div>
-                       
-                        
                         <div className={`div-grabaciones-${subtitulo} div-grabaciones`}>
-                           
-                        { grabacion 
+                            { grabacion 
                                 ?  
                                 <> 
-                                
                                     <iframe 
                                         src={`https://drive.google.com/file/d/${ grabacion }/preview`} 
                                         width="410" 
                                         height="60" 
                                         allow="autoplay"
                                         className={`iframe-${subtitulo}-${index}`}>
-                                        </iframe>
+                                    </iframe>
                                 </>
-                                    
                                 : 
                                 <> Todavia no hay ninguna Grabación </>
-                        }
-
-  
+                            }
                             <div className={`div-grabacion-${subtitulo}-${index}`}>
                                 <audio src={""} className={`grabacion-${subtitulo}-${index}`}  />
                             </div>
-                            
-                  
                             {
                                 document.querySelector(`.iframe-${subtitulo}-${index}`)
                                     ? <>
                                       <button className="button" onClick={(e) => actualizarGrabacion(e.target.previousElementSibling) } > 
                                         Actualizar Grabación 
                                       </button>
-                                 
                                     </>
-                                  
-                                
                                     : document.querySelector(`.grabacion-${subtitulo}-${index}`) 
                                         ? <button className="button" onClick={(e) => guardarGrabacion(e.target.previousElementSibling, index, id) } > 
-                                          Guardar Grabación
+                                            Guardar Grabación
                                         </button>
                                         : <button className="button"> No hay grabacion! </button>
                             }
-                            
-                            
                         </div>
                     </section>
-
                 </section>
-
             </article>                      
-          
         </>
     )
 }
 
-export default Clip
+export default Clip;
