1b8d8951302b12c13ff397ae96e62b1c025d2dfd
geronimo <geronicola1696@gmail.com>
2024-05-21 00:05:53 -0300

    shadowing y mejoras SEO

--- src/Components/Index/Index.jsx
+++ src/Components/Index/Index.jsx
@@ -75,31 +75,45 @@ function App() {
               
             </li>
     
-      
+
             <li className="flex wrap li-index fondo-li-3 p-30 flex-column-center" >
+              <h2> Shadowing </h2>
+              <h3> El shadowing es una técnica de aprendizaje de idiomas en la que que se busca repetir un audio justo después de escucharlo. La idea es tratar de actuar como un "eco" o una "sombra" (de ahí el nombre "shadowing). 
+                Se escucha, en base a ello se trata de repeir las palabras en voz alta. 
+              </h3>
+              <h2> ¿Cómo ayuda el shadowing? </h2>
+              <h3> 
+              El objetivo principal de la técnica del shadowing es el de familiarizarse con el idioma extranjero. A la larga se busca acostumbrarse eficazmente al sonido del idioma extranjero cuando se lo repita. 
+              Esto te ayudará especialmente a entrenar tu comprensión auditiva. El shadowing también ayuda a mejorar la pronunciación. Cuando haces shadowing, entrenas los movimientos de la lengua y boca típicos de tu idioma extranjero. Si aprendes los patrones de tu lengua extranjera, podrás utilizarlos mejor y d
+              </h3>
+
+          
+            </li>
+      
+            <li className="flex wrap li-index fondo-li-4 p-30 flex-column-center" >
             
-              <h4 className="p-index">   Para clasificar la dificultad de los clips traté de guiarme por mi propia comprensión de los mismos, pero teniendo en cuenta también otros factores importantes.
+              <h5 className="p-index">   Para la clasificacion de la dificultad de los clips se trato de guiarme por mi propia comprensión de los mismos, pero teniendo en cuenta también otros factores importantes.
                Entre ellos: La complejidad de la situación en la cual los dialogos se desarrollan; La complejidad del vocabulario utilizado; El grado de dependencia que puedan llegar de datos externos o previos;
               La forma en la que los personajes se expresan, su claridad, acento, modismos, etc. 
-              </h4>
-              <h4 className="p-index"> Para que la actividad resulte efectiva se recomienda buscar clips de series/peliculas que puedan 
+              </h5>
+              <h5 className="p-index"> Para que la actividad resulte efectiva se recomienda buscar clips de series/peliculas que puedan 
                 estar acorde a su nivel. Así como también haber visto la pelicula o el capitulo de interés con anterioridad. Tener un contexto de la situacion en la que el clip se desarrolla 
                 aporta informacion valiosa al momento de comprender diálogos que quizas aisladamente puedan resultar muy complicados o imposibles.
-              </h4>
+              </h5>
 
             </li>
 
             
-            <li className="flex wrap li-index fondo-li-4 p-30 flex-column-center" >
+            <li className="flex wrap li-index fondo-li-5 p-30 flex-column-center" >
             
-              <h4 className="p-index">
+              <h6 className="p-index">
                Para cada clip se recomienda escucharlo varias veces y luego tratar de replicar lo entendido grabandolo en audio. Tambien 
                se puede evaluar su comprension escribiendo en el campo de texto correspondiente y tocar el boton "Evaluar" para verificarlo.
-              </h4>
+              </h6>
 
-              <h4 className="p-index"> La opcion "Mostrar Respuesta" siempre esta disponible; Para realizar grabaciones, debera registrase. Una vez tenga un usuario 
+              <h6 className="p-index"> La opcion "Mostrar Respuesta" siempre esta disponible; Para realizar grabaciones, debera registrase. Una vez tenga un usuario 
                las grabacaciones podran guardarse, tanto como actualizarse o borrarse. 
-              </h4>
+              </h6>
           
             </li>
             
